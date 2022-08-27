235b8491d8329052e4cf0d344a62a1ce00bbbf09
Exiro <exirodev@gmail.com>
2022-08-27 13:45:37 +0100

    Initital commit

---
+++ src/main/java/com/exiro/reader/sgBitmapRecord.java
@@ -0,0 +1,15 @@
+package com.exiro.reader;
+
+public class sgBitmapRecord {
+
+    String filename;
+    String comment;
+    long width;
+
+    long height;
+    long num_images;
+    long start_index;
+    long end_index;
+
+
+}
